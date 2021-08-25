3372b0081f64f95c8ecdad8357d7927d020881df
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2021-08-25 12:12:19 -0700

    Add versioneer

--- impact/__init__.py
+++ impact/__init__.py
@@ -2,9 +2,11 @@ from .impact import Impact
 from .impact_distgen import run_impact_with_distgen, evaluate_impact_with_distgen
 from .control import ControlGroup
 
-from ._version import __version__
 
 import os
 # Used to access data directory
 root, _ = os.path.split(__file__)
 template_dir = os.path.join(root, '../templates/')
+
+from . import _version
+__version__ = _version.get_versions()['version']
