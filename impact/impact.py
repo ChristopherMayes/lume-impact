1477fa804974c27efd68de5508fdb036717cd60f
ChristopherMayes <31023527+ChristopherMayes@users.noreply.github.com>
2021-09-09 09:10:42 -0700

    Initial particles from the cathode will now set the Tini directly, not shift. This means that the initial particle times must be set relative to the absolute clock.

--- impact/impact.py
+++ impact/impact.py
@@ -417,13 +417,8 @@ class Impact:
         if update_header:
             for k, v in res.items():
                 if k in H:
-                    # Special case to shift initial time with cathode start 
-                    if k == 'Tini' and self.cathode_start:
-                        H[k] += v
-                        self.vprint(f'{start_str}: Shifted {k} by {v} according to initial particles')  
-                    else:
-                        H[k] = v
-                        self.vprint(f'{start_str}: Replaced {k} with {v} according to initial particles')    
+                    H[k] = v
+                    self.vprint(f'{start_str}: Replaced {k} with {v} according to initial particles')    
            
             # These need to be set
             H['Flagdist'] = 16
