2f02757686795cdd7a48b4c0a4ffc344f608e937
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2024-01-03 13:32:30 -0800

    DEV: migrate to pyproject.toml and setuptools-scm

--- impact/__init__.py
+++ impact/__init__.py
@@ -1,7 +1,15 @@
-from . import _version
-__version__ = _version.get_versions()['version']
-
 from .impact import Impact
 from .impact_distgen import run_impact_with_distgen, evaluate_impact_with_distgen
 from .control import ControlGroup
 
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0.0"
+
+__all__ = [
+    "Impact",
+    "run_impact_with_distgen",
+    "evaluate_impact_with_distgen",
+    "ControlGroup",
+]
