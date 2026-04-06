0408432314613ca9a47d935f98d85705beedf27b
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2026-04-06 16:55:03 -0700

    FIX: polars may raise SchemaError now

--- impact/z/particles.py
+++ impact/z/particles.py
@@ -134,7 +134,7 @@ class ImpactZParticles(BaseModel):
                     .to_numpy()
                     .T
                 )
-            except pl.exceptions.ComputeError:
+            except (pl.exceptions.ComputeError, pl.exceptions.SchemaError):
                 fp.seek(start_pos)
                 lines = fp.read().splitlines()
                 (x, px, y, py, phase, pz, charge_to_mass_ratio, weight, id) = (
